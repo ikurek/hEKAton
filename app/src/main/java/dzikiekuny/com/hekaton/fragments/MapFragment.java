996ad3df5a604d486c06439c0ff511dac7947730
Bartosz Woźniak <bartoszwoz0@gmail.com>
2017-06-02 22:48:12 +0200

    MapFragment in MainActivity

--- app/src/main/java/dzikiekuny/com/hekaton/fragments/MapFragment.java
+++ app/src/main/java/dzikiekuny/com/hekaton/fragments/MapFragment.java
@@ -8,13 +8,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import dzikiekuny.com.hekaton.R;
 
 /**
  * Created by wodzu on 02.06.17.
  */
 
-public class MapFragment  extends Fragment {
+public class MapFragment extends Fragment {
+
+    private MapView mapView;
+    private GoogleMap googleMap;
+
+    private LatLng slodowa = new LatLng(51.116162, 17.037725);
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +42,34 @@ public class MapFragment  extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.map_fragment, container, false);
 
+        mapView = (MapView) rootView.findViewById(R.id.map_fragment);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.onResume(); // needed to get the map to display immediately
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                // googleMap.setMyLocationEnabled(true); //TODO: uprawnienia do lokalizacji
+
+                //Marker pozycji wyspy
+                googleMap.addMarker(new MarkerOptions().position(slodowa).title("Slodowa").snippet("Słodowa w chuj").icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(slodowa).zoom(16).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            }
+        });
         return rootView;
+
     }
 }
